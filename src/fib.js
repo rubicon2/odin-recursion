eb5ab2d3936aa08ba241ab72b42beb5cbf50d480
rubicon2 <liargent@hotmail.com>
2024-02-05 14:54:41 +0000

    Write non-recursive fib function

--- src/fib.js
+++ src/fib.js
@@ -1,4 +1,12 @@
-export default function fib(n) {
+export function fib(n) {
+  const fibs = [];
+  for (let current = 0; current < n; current += 1) {
+    fibs.push(fibs[current - 1] + fibs[current - 2] || 1);
+  }
+  return fibs[fibs.length - 1];
+}
+
+export function fibRec(n) {
   if (n <= 2) return 1;
   return fib(n - 1) + fib(n - 2);
 }
