fa21b6d6e4412013c884f3efb595300053158c17
rubicon2 <liargent@hotmail.com>
2024-02-06 16:43:51 +0000

    Analyze time complexity of fib functions

--- src/fib.js
+++ src/fib.js
@@ -1,24 +1,34 @@
+// Total: O(1) * 3 + O(n), i.e. O(n) - linear
 export function fibList(n) {
-  const fibs = [0, 1];
+  const fibs = [0, 1]; // O(1)
   for (let current = fibs.length; current <= n; current += 1) {
-    fibs.push(fibs[current - 1] + fibs[current - 2]);
+    // O(n)
+    fibs.push(fibs[current - 1] + fibs[current - 2]); // O(1)
   }
-  return fibs;
+  return fibs; // O(1)
 }
 
+// Uses fibList function which is O(n) - linear
 export function fib(n) {
   return fibList(n)[n];
 }
 
+// Total: O(1) * 3 + O(n - 1), i.e. O(n) - linear
 export function fibListRec(n) {
-  if (n < 2) return [0, 1];
-  const prior = fibListRec(n - 1);
+  if (n < 2) return [0, 1]; // O(1)
+  const prior = fibListRec(n - 1); // O(n - 1)
   // Due to base case, prior will always consist of at least [0, 1]
-  const current = prior[n - 1] + prior[n - 2];
-  return [...prior, current];
+  const current = prior[n - 1] + prior[n - 2]; // O(1)
+  return [...prior, current]; // O(1)
 }
 
+// Uses fibListRec function which is O(n) - linear
+export function fibRecFast(n) {
+  return fibListRec(n)[n];
+}
+
+// Total: O(1) + O(2n), i.e. O(2^n) - exponential
 export function fibRec(n) {
-  if (n < 2) return n;
-  return fibRec(n - 1) + fibRec(n - 2);
+  if (n < 2) return n; // O(1)
+  return fibRec(n - 1) + fibRec(n - 2); // O(2n)
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,50 @@
 import './style.css';
-import { fib, fibRec } from './fib';
+import { fib, fibRec, fibList, fibListRec, fibRecFast } from './fib';
+import mergeSort from './mergeSort';
 
 const fibRecTest = fibRec(8);
 console.log('fibRec 8: ', fibRecTest);
 
 const fibTest = fib(8);
 console.log('fib 8: ', fibTest);
+
+const fibListR = fibListRec(8);
+console.log(fibListR);
+
+const fibListNormal = fibList(8);
+console.log(fibListNormal);
+
+const sorted = mergeSort([105, 79, 100, 110]);
+console.log('sorted: ', sorted);
+
+const mergeSortArbitrary = mergeSort(
+  [
+    {
+      name: 'bob',
+      age: 31,
+      height: 6.2,
+    },
+    {
+      name: 'glob',
+      age: 19,
+      height: 5.4,
+    },
+    {
+      name: 'schblob',
+      age: -47,
+      height: 5.11,
+    },
+  ],
+  (a, b) => a.age < b.age,
+);
+console.log(mergeSortArbitrary);
+
+console.time('fibRec');
+// Took 2937ms...
+console.log(fibRec(40));
+console.timeEnd('fibRec');
+
+console.time('fibRecFast');
+// Took 0ms!
+console.log(fibRecFast(40));
+console.timeEnd('fibRecFast');
